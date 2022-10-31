710756f694f6de35473d45b52ceefaf0d3b0566c
mcmillenchrisj <mcmillenchrisj@gmail.com>
2022-10-31 00:33:46 -0400

    saving old branch before merging

--- project-2-react-app/src/shared/store.ts
+++ project-2-react-app/src/shared/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserSlicer from "../components/Login/UserSlicer";
+import CartSlicer from "../components/Pages/Cart/CartSlicer";
 
 export const store = configureStore({
     reducer: {
         user: UserSlicer,
+        product: CartSlicer,
     }
 })
 
