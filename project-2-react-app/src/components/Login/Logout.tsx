4ea5eb9cb3c3359779cc2c7fd8218ebcbecb48a3
Aideash <shaferai210@gmail.com>
2022-10-31 07:14:56 -0700

    stand-up halloween

---
+++ project-2-react-app/src/components/Login/Logout.tsx
@@ -0,0 +1,18 @@
+import { Container } from "react-bootstrap";
+import { IUserModel } from "../models/userModel";
+
+export function Logout(){
+    
+    const emptyUser:IUserModel = {
+        id:0,
+        firstName:"",
+        lastName:"",
+        email:"",
+        role:0,
+    } 
+    
+    
+    return <Container>
+        
+    </Container>
+}
